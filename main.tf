ec9f57b5280f2d1638fb4f1684df35605609e86f
skcanada6333 <111817606+skcanada6333@users.noreply.github.com>
2022-12-07 20:21:30 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 provider "azurerm" {
+   subscription_id = "d5cfd521-b891-4b93-a71d-4f7d88dabf46"
+    client_id = "c4abd404-ba36-4e77-abc8-3c80d7219aee"
+    client_secret = "K7S8Q~I2dF3jvwj86sPr2Qs5o6K3E6sf2Dz4PaGP"
+    tenant_id = "d6aeaca1-6481-4fb5-a45a-fd02b4ea93c9"
 features {}
 }
 # cfreate resource group
